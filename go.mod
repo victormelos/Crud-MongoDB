87f8f7c08ee30213f7f3ab447c04e691fed53177
Victor Melo <victormelotrabalho1@gmail.com>
2025-02-25 12:16:39 -0300

    No changes made

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/victormelos/curso-youtube
+
+go 1.23.5
+
+require github.com/joho/godotenv v1.5.1 // indirect
---
+++ src/configuration/rest_err/rest_err.go
@@ -0,0 +1,32 @@
+package rest_err
+
+import "net/http"
+
+type RestErr struct {
+	Message string  `json:"message"`
+	Err     string  `json:"error"`
+	Code    int     `json:"code"`
+	Causes  []Cause `json:"causes"`
+}
+
+type Cause struct {
+	Field   string `json:"field"`
+	Message string `json:"message"`
+}
+
+func NewRestErr(message, err string, code int, causes []Cause) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     err,
+		Code:    code,
+		Causes:  causes,
+	}
+}
+func NewBadRequestError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "bad_request",
+		Code:    http.StatusBadRequest,
+		Causes:  []Cause{},
+	}
+}
